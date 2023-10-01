615104fe166dfceab3ef4b3a3618c8a1477b637b
Michael Beierwaltes <mbeierwaltes321@gmail.com>
2023-10-01 16:48:42 -0500

    Created solution to check for the API token.
    TODO:
    	1. Test for when the API Token expires
    	2. Replaced the hardcoded "1" to when the Spotify API expires

--- backend/src/main/java/com/mbmusic/backend/Connections/SpotifyApiConnection.java
+++ backend/src/main/java/com/mbmusic/backend/Connections/SpotifyApiConnection.java
@@ -1,5 +1,7 @@
 package com.mbmusic.backend.Connections;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Component;
 
 import se.michaelthelin.spotify.SpotifyApi;
@@ -14,13 +16,16 @@ public class SpotifyApiConnection {
     //#region " Members "
         
     //The client ID used for generating the access token
-    private final String clientId = System.getenv("SpotifyClientId");
+    private static final String clientId = System.getenv("SpotifyClientId");
 
     //The client secret used for generating the access token
-    private final String clientSecret = System.getenv("SpotifyClientSecret");
+    private static final String clientSecret = System.getenv("SpotifyClientSecret");
+
+    //The time date and time which the access token was last generated
+    private static LocalDateTime tokenAccessed;
 
     //The api object that contains the connection information
-    private final SpotifyApi apiClient = new SpotifyApi.Builder()
+    private static final SpotifyApi apiClient = new SpotifyApi.Builder()
         .setClientId(clientId)
         .setClientSecret(clientSecret)
         .build();
@@ -33,23 +38,44 @@ public class SpotifyApiConnection {
     //#region " Methods "
     
     //Getter for the Spotify API client, first refreshes the access token
-    //TODO - Eventually phase this out in favor for a scheduled trigger on refreshing the access token
     public SpotifyApi getApiClient() {
+
+        //First, check if the access token exists, or if it expired
+        if (apiClient.getAccessToken() == null || java.time.LocalDateTime.now().isAfter(tokenAccessed.plusHours(1))) {
+            if(refreshAccessToken() == false) {
+                //Return null, there was an error setting the credentials
+                return null;
+            }
+        }
+        
+        return apiClient;
+    }
+
+    //This method refreshes the access token for the api client
+    private boolean refreshAccessToken() {
+
+        //First create a return variable
+        boolean refreshSuccessful = false;
 
         try {
             //First grab the credentails, and set the access token
             ClientCredentials creds = clientCredentialsRequest.execute();
             apiClient.setAccessToken(creds.getAccessToken());
+
+            //Update the access token time
+            tokenAccessed = java.time.LocalDateTime.now();
+
+            //Success, set the return variable to true
+            refreshSuccessful = true;
         } catch (Exception e) {
             // Print out exception
             System.out.println(e.getMessage());
             
-            //Failed, so return null;
-            return null;
+            //Failed, so return variable remains false
         }
 
-        return apiClient;
-    }
+        return refreshSuccessful;
+    } 
 
 
     //#endregion
--- backend/src/main/java/com/mbmusic/backend/Playlists/PlaylistController.java
+++ backend/src/main/java/com/mbmusic/backend/Playlists/PlaylistController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mbmusic.backend.Connections.SpotifyApiConnection;
 
-import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.model_objects.specification.Artist;
 import se.michaelthelin.spotify.requests.data.artists.GetArtistRequest;
 
 //This class resembles the controller for accessing playlist data
@@ -15,14 +15,14 @@ public class PlaylistController {
     //#region " Members "
     
     //The client to the spotify API
-    private final SpotifyApi spotifyClient;
+    private final SpotifyApiConnection spotifyClient;
 
     //#endregion
 
     //#region " Constructor "
 
     public PlaylistController(SpotifyApiConnection connection) {
-        this.spotifyClient = connection.getApiClient();
+        this.spotifyClient = connection;
     }
 
     //#endregion
@@ -34,16 +34,17 @@ public class PlaylistController {
     @RequestMapping("test")
     public String GetTest() {
         
-        GetArtistRequest request = this.spotifyClient.getArtist("1GxkXlMwML1oSg5eLPiAz3").build();
-        String artist;
+        GetArtistRequest request = this.spotifyClient.getApiClient().getArtist("1GxkXlMwML1oSg5eLPiAz3").build();
+        Artist artist;
 
         try {
-            artist = request.execute().getName();
-        } catch (Exception e) {
+            //Attempt to grab the artist
+            artist = request.execute();
+        } catch (Exception e) {            
             System.out.println(e.getMessage());
             return "Oops!";
         }
-        return "I LOVE " + artist;
+        return "I LOVE " + artist.getName();
     }
 
     //#endregion
