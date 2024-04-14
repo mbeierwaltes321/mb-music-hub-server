23203c27342691851b0e56f160b6d308e4d5896e
Michael Beierwaltes <mbeierwaltes321@gmail.com>
2024-04-14 16:51:54 -0500

    Created draft of introduction page

---
+++ mb-music-hub-frontend/mb-music-hub-frontend/src/components/Introduction.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div id="IntroContainer" class="container text-center">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-light">Hi! 👋</h1>
+            </div>
+        </div>
+        <div class="row">
+            <a href="#" class="fs-2 text-primary mt-md-3">Who am I?</a>
+        </div>
+        <div class="row">
+            <button type="button" class="btn btn-primary mt-md-5"> Let's Go! </button>
+        </div>
+    </div>
+</template>
