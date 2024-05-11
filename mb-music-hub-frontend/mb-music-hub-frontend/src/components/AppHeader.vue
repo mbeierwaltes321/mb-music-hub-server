f6819336c7465740ff1f563f1ffa969fcc68168d
Michael Beierwaltes <mbeierwaltes321@gmail.com>
2024-05-10 23:56:03 -0500

    Added login icons, nearly finished toolbar, just have to fix issue with hamburger menu aligned with the login button

--- mb-music-hub-frontend/mb-music-hub-frontend/src/components/AppHeader.vue
+++ mb-music-hub-frontend/mb-music-hub-frontend/src/components/AppHeader.vue
@@ -6,9 +6,6 @@
     <nav id="NavigationBar" class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">MB's Music Hub</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMediaSelection" aria-controls="navbarSupportedContent" aria-expanded="false">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <div class="collapse navbar-collapse" id="navbarMediaSelection">
                 <ul class="navbar-nav">
                     <li class="nav-item">
@@ -26,6 +23,12 @@
                     </li>
                 </ul>
             </div>
+            <div id="spotifyLoginButton" class="d-flex flex-row-reverse">
+                <img class="bg-light" src="./../assets/stadium-light-plain.svg">
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMediaSelection" aria-controls="navbarSupportedContent" aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
     </nav>
 </template>
