766d58fe4d7d79c158a5c751ae2248e8ed3517aa
Michael Beierwaltes <mbeierwaltes321@gmail.com>
2024-05-10 22:20:13 -0500

    App Header checkpoint 1

--- mb-music-hub-frontend/mb-music-hub-frontend/src/App.vue
+++ mb-music-hub-frontend/mb-music-hub-frontend/src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import Introduction from "./components/Introduction.vue"
+import Introduction from "./components/Introduction.vue";
+import AppHeader from "./components/AppHeader.vue";
 </script>
 
 <template>
-  <!-- TODO- Define header -->  
-  <Introduction class="d-flex justify-content-center"></Introduction>
+    <AppHeader id="app-header" class="d-flex "/>
+    <Introduction class="d-flex justify-content-center h-auto"></Introduction>
   <!-- TODO - Define footer -->
 </template>
+
+<style scoped>
+#app-header {
+  min-height: 15vh;
+}
+</style>
---
+++ mb-music-hub-frontend/mb-music-hub-frontend/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav id="NavigationBar" class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">MB's Music Hub</a>
+            <div class="collapse navbar-collapse" id="navbarMediaSelection">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+#app-logo {
+    width: 50px;
+    height: 50px;
+}
+</style>
