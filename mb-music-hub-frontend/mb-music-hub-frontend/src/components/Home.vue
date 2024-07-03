46641c0032732b2841f927243bf1df6209307c69
Michael Beierwaltes <mbeierwaltes321@gmail.com>
2024-07-02 21:28:02 -0500

    Built home component using Vuetify. Set up base to start working on the home component

--- mb-music-hub-frontend/mb-music-hub-frontend/src/components/Home.vue
+++ mb-music-hub-frontend/mb-music-hub-frontend/src/components/Home.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-import { VCard } from 'vuetify/components';
+import { VCard, VContainer, VCol, VRow } from 'vuetify/components';
+import AppFooter from './AppFooter.vue';
+import AppHeader from './AppHeader.vue';
+
 </script>
 
 <template>
-    <v-card text="Ciao mondo!">
+    <v-container id="HomeContainer" :fluid="true" class="p-0">
+        <v-row no-gutters class="h-100">
+            <v-col cols="12" class="position-fixed">
+                <AppHeader />                
+            </v-col>
+            <v-col cols="12">
+                <div style="height: 400px; width: 2000px; ">
 
-    </v-card>
+                </div>
+            </v-col>
+            <v-col cols="12" align-self="end">
+                <AppFooter />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
+
+<style scoped>
+
+#HomeContainer {
+  height: 100vh;
+}
+
+</style>
