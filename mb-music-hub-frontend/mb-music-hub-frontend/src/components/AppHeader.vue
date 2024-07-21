5561e99ed6ca913cc5821380219bb00baef91bae
Michael Beierwaltes <mbeierwaltes321@gmail.com>
2024-07-20 22:52:35 -0500

    Made the app header responsive; added hamburger menu for mobile devices
    Update AppHeader.vue

--- mb-music-hub-frontend/mb-music-hub-frontend/src/components/AppHeader.vue
+++ mb-music-hub-frontend/mb-music-hub-frontend/src/components/AppHeader.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { VAppBar, VMenu, VAppBarTitle, VBtn } from "vuetify/components";
-import { type Ref, ref } from "vue";
+import { VAppBar, VMenu, VAppBarTitle, VBtn, VAppBarNavIcon } from "vuetify/components";
+import { type Ref, ref, watch } from "vue";
+import { useDisplay } from "vuetify";
 
-const theme : Ref<string | null> = ref(localStorage.getItem("colorTheme"));
+const theme: Ref<string | null> = ref(localStorage.getItem("colorTheme"));
 
 //TODO - Eventually frame this so that it works with Vue router.
 const mediaItems = [
@@ -11,28 +12,33 @@ const mediaItems = [
     { title: "YouTube Mixes", value: 2 }
 ];
 
+//Determine if the current screen is in the medium breakpoint or higher (960px or greater)
+const { mdAndUp } = useDisplay();
+const drawer: Ref<boolean | null> = ref<boolean | null>(null);
+
+//Add watch that closes the drawer if the screen changes
+watch(() => mdAndUp.value, () => {
+    drawer.value = false;
+})
+
 </script>
 
 <template>
-    <!--TODO: Solve sizing issue in mobile. Title gets minimized-->
     <v-app-bar>
+        <v-app-bar-nav-icon v-if="!mdAndUp" @click.stop="drawer = !drawer" />
         <v-app-bar-title>MB's Music Hub</v-app-bar-title>
-        <v-menu>
-            <template #activator="{props}">
-                <v-btn v-bind="props"
-                       variant="text"
-                       class="text-none"
-                       append-icon="fas fa-caret-down"
-                       size="x-large">Media</v-btn>
+        <v-menu v-if="mdAndUp">
+            <template #activator="{ props }">
+                <v-btn v-bind="props" variant="text" class="text-none" append-icon="fas fa-caret-down"
+                    size="large">Media</v-btn>
             </template>
             <v-list :items="mediaItems" />
         </v-menu>
-        <v-btn class="text-none"
-               variant="outlined"
-               rounded="xl"
-               append-icon="fab fa-spotify"
-               size="x-large">
-               Login
+        <v-btn class="text-none" variant="outlined" rounded="xl" append-icon="fab fa-spotify" size="x-large">
+            Login
         </v-btn>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" temporary>
+        <v-list :items="mediaItems" />
+    </v-navigation-drawer>
 </template>
