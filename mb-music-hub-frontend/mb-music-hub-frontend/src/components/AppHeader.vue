30eda3094cf4ec240ecff273df2562c31247b1c9
Michael Beierwaltes <mbeierwaltes321@gmail.com>
2024-07-24 20:41:47 -0500

    Added theme toggle button to navigation drawer

--- mb-music-hub-frontend/mb-music-hub-frontend/src/components/AppHeader.vue
+++ mb-music-hub-frontend/mb-music-hub-frontend/src/components/AppHeader.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
-import { VAppBar, VMenu, VAppBarTitle, VBtn, VAppBarNavIcon } from "vuetify/components";
-import { type Ref, ref, watch } from "vue";
+import { VAppBar, VMenu, VAppBarTitle, VBtn, VAppBarNavIcon, VList, VListItem } from "vuetify/components";
+import { computed, type Ref, ref, watch } from "vue";
 import { useDisplay, useTheme } from "vuetify";
 import { setThemeInLocalStorage } from "@/utilities/localStorage";
 
 const theme = useTheme();
 
-//TODO - Eventually frame this so that it works with Vue router.
-//TODO - Add the light/dark theme toggle depending on the size of the screen
-const mediaItems = [
-    { title: "Playlists", value: 0 },
-    { title: "Podcasts", value: 1 },
-    { title: "YouTube Mixes", value: 2 }
-];
-
 //Determine if the current screen is in the medium breakpoint or higher (960px or greater)
 const { mdAndUp } = useDisplay();
 const drawer: Ref<boolean | null> = ref<boolean | null>(null);
+
+//TODO - Eventually frame this so that it works with Vue router.
+const mediaItems = computed(() => [
+    { title: "Playlists", value: 0 },
+    { title: "Podcasts", value: 1 },
+    { title: "YouTube Mixes", value: 2 }
+]);
 
 
 /**
@@ -46,13 +45,22 @@ watch(() => mdAndUp.value, () => {
             </template>
             <v-list :items="mediaItems" />
         </v-menu>
-        <v-btn :icon="theme.current.value.dark ? 'fas fa-moon' : 'far fa-sun'" 
+        <v-btn v-if="mdAndUp" 
+               :icon="theme.current.value.dark ? 'fas fa-moon' : 'far fa-sun'" 
                @click="toggleTheme"/>
         <v-btn class="text-none" variant="outlined" rounded="xl" append-icon="fab fa-spotify" size="x-large">
             Login
         </v-btn>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" temporary>
-        <v-list :items="mediaItems" />
+        <v-list>
+            <v-list-item v-for="item in mediaItems"
+                         :key="item.value"
+                         :title="item.title"/>
+            <v-list-item :key="mediaItems.length"
+                         :prepend-icon="theme.current.value.dark ? 'fas fa-moon' : 'far fa-sun'"
+                         :ripple="false"
+                         @click="toggleTheme" />
+        </v-list>
     </v-navigation-drawer>
 </template>
