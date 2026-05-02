ca1d80b4708daf06f79a531d65785a274b9be6cc
Michael Beierwaltes <mbeierwaltes321@gmail.com>
2026-05-01 23:56:58 -0500

    Added postSpotifyLogin_shouldReturnError

--- src/test/java/com/mbmusic/hubserver/Connections/ConnectionControllerTests.java
+++ src/test/java/com/mbmusic/hubserver/Connections/ConnectionControllerTests.java
@@ -1,27 +1,27 @@
 package com.mbmusic.hubserver.Connections;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.net.URI;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.invocation.InvocationOnMock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.mbmusic.hubserver.BaseTest;
+import com.mbmusic.hubserver.Connections.Exceptions.SpotifyAuthorizationException;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -45,9 +45,11 @@ public class ConnectionControllerTests extends BaseTest {
 
     //#region Tests
 
-    @BeforeAll
-    private static void prepareMockGateway() {
+    @BeforeEach
+    private void prepareMockGateway() {
         mockApiGateway = mock(SpotifyApiGateway.class);
+        when(mockApiConnection.createEmptySpotifyApiGateway())
+            .thenReturn(mockApiGateway);
     }
 
     //Sanity check
@@ -59,9 +61,9 @@ public class ConnectionControllerTests extends BaseTest {
     /**
      * This is the test plan. We need to write tests that do the following
      * 1. Ensure the context loads --DONE--
-     * 2. POST spotifylogin tests --DONE--
-     *  2.1. Success - Returns a successful redirect
-     *  2.2. Failure - Authorization URI returns a failure and it throws a SpotifyAuthorizationException
+     * 2. POST spotifylogin tests
+     *  2.1. Success - Returns a successful redirect --DONE--
+     *  2.2. Failure - Authorization URI returns a failure and it throws a SpotifyAuthorizationException --DONE--
      *  2.3. Failure - State returned from URI does not match, and it throws a SpotifyAuthorizationException
      * 3. GET redirect tests
      *  3.1. Success - We get a successful redirect to the front end
@@ -79,8 +81,6 @@ public class ConnectionControllerTests extends BaseTest {
     public void postSpotifyLoginSuccess() throws Exception {
         String URI_WITHOUT_STATE = "http://testuri.com/";
 
-        when(mockApiConnection.createEmptySpotifyApiGateway())
-            .thenReturn(mockApiGateway);
         when(mockApiGateway.createAuthorizationURI(anyString()))
             .thenAnswer((InvocationOnMock invoation) -> {
                 String state = invoation.getArgument(0);
@@ -90,6 +90,28 @@ public class ConnectionControllerTests extends BaseTest {
         
         mvc.perform(post("/conn/spotifylogin"))
             .andExpect(status().isFound());
+    }
+
+    /**
+     * This method tests the scenario where the authorization request returns an error
+     * @throws Exception
+     */
+    @Test
+    public void postSpotifyLogin_shouldReturnError() throws Exception {
+        URI URI_WITH_ERROR = new URI("http://testuri.com/?state=Hawaii&error=failure");
+
+        when(mockApiGateway.createAuthorizationURI(anyString()))
+            .thenReturn(URI_WITH_ERROR);
+
+        var exception = mvc.perform(post("/conn/spotifylogin"))
+            .andExpect(status().isInternalServerError())
+            .andReturn()
+            .getResolvedException();
+
+        assertInstanceOf(SpotifyAuthorizationException.class, exception);
+        assertTrue(exception.getMessage().contains("Access denied"));
+        assertTrue(exception.getMessage().contains("failure"));
+
     }
 
     //#endregion
